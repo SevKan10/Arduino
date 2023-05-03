75b12cfe82ae9b27ccfee265b4f93a198a12d12d
KKhang <khangkhuu10727@gmail.com>
2023-05-04 00:19:22 +0700

    oled

---
+++ oled/oled.ino
@@ -0,0 +1,18 @@
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello World!");
+  display.display();
+}
+
+void loop() {
+}
