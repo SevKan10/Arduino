09abe079de0abfcfc2695631ecbe9616010051f4
KKhang <khangkhuu10727@gmail.com>
2023-05-07 00:35:35 +0700

    eeprom

--- MPU6050_code/MPU6050_code.ino
+++ MPU6050_code/MPU6050_code.ino
@@ -1,10 +1,10 @@
 #include <MPU6050_tockn.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
+#include <EEPROM.h>
 MPU6050 mpu6050(Wire);
 LiquidCrystal_I2C lcd (0x27,16,2);
-
+int addr=0;
 void setup() {
   lcd.init();
   lcd.backlight();
@@ -16,18 +16,33 @@ void setup() {
 
 void loop() 
 {
+
   mpu6050.update();
   float x= mpu6050.getAccX();
   float y = mpu6050.getAccY();
   float z = mpu6050.getAccZ();
-  Serial.print("x: "); Serial.print(x);
+  
+    if (mpu6050.getAccY() !=0)
+  {
+    Serial.print("x: "); Serial.print(x);
   Serial.print("\ty: "); Serial.print(y);
   Serial.print("\tz: "); Serial.println(z);
-  delay(200);
+  delay(500);
   if (y>0.90)
   {
     Serial.println("xe bi nga ");
   }
     lcd.setCursor(0,0);
     lcd.print("y: ");lcd.print(y);
+  }
+  else
+  {
+    EEPROM.write(addr,x);
+ //   Serial.print(addr);
+    addr++;
+    if (addr == 1024)
+    addr = 0;
+
+  delay(5);
+  }
 }
