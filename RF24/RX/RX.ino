e819b0b83fcb68044e348b870632a126c9e5a149
KKhang <khangkhuu10727@gmail.com>
2023-06-24 21:26:01 +0700

    reup

--- RF24/RX/RX.ino
+++ RF24/RX/RX.ino
@@ -4,15 +4,16 @@
 RF24 radio(9, 10); // CE, CSN
 const byte address[6] = "2210";
 //-----------------------------
+struct package_receive{ int TX_Connect; };
+package_receive data_receive;
+
+int flag1;
 int receivedData1;
 int receivedData2;
-int in1 = 2;
-int in2 = 3;
-int in3 = 4;
-int in4 = 7;
-int enC = 6;
-int enG = 5;
+int led = 5;
+
 unsigned long time;
+unsigned long ping = 0;   
 
 void setup() {
   Serial.begin(9600);
@@ -23,25 +24,40 @@ void setup() {
   }
   radio.openReadingPipe(1, address);
   radio.setPALevel(RF24_PA_HIGH);
+  radio.setChannel(2);
   radio.setDataRate(RF24_250KBPS);
   radio.startListening();
 
-  pinMode(enG, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT); 
-  pinMode(enC,OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
   while (radio.available()) {
     radio.read(&receivedData1, sizeof(receivedData1));
     radio.read(&receivedData2, sizeof(receivedData2));
+    radio.read(&data_receive, sizeof(package_receive)); 
+    ping = 0; flag1=0;
     Serial.print("Received data:  ");
     Serial.print(receivedData1);
-    Serial.print("      ");
+    Serial.print("\t");
     Serial.println(receivedData2);
+    
   }
-  Run();
+ if(receivedData1>900){
+   if (millis() - time >=10){
+   for (int i=0;i<256;i++){
+   digitalWrite(led, i);
+   time = millis();
+   }
+   }
+ } 
+
+  while(!radio.available())
+  {
+    ping++; 
+    if(ping/1000>50 )
+    {
+      Serial.println("Disconnected");
+    }
+  }
 }
--- RF24/TX/TX.ino
+++ RF24/TX/TX.ino
@@ -3,7 +3,11 @@
 #include <RF24.h>
 RF24 radio(9, 10); // CE, CSN
 const byte address[6] = "2210";
-//----------------------------
+//--------------------------------------------------------Cấu trúc Data
+struct package_send{ int TX_Connect; };
+package_send data_send;
+
+unsigned long interrupt;
 int joyX1 = A3;  //Chạy tiến, lùi
 int joyY2 = A2; // Đánh lái
 int led = 7;
@@ -13,13 +17,10 @@ unsigned long time;
 void setup() {
   Serial.begin(9600);
   radio.begin();
-  if (radio.begin()) {
-    digitalWrite(led,1);delay(5000);
-    digitalWrite(led,0);
-
-  }
+  if (!radio.begin()) {Serial.print("RF DON'T START");}
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_HIGH);
+  radio.setChannel(2);
   radio.setDataRate(RF24_250KBPS);
 
   pinMode(joyX1,INPUT);
@@ -29,13 +30,19 @@ void setup() {
 void loop() {
   if (millis() - time >1000)
   {
-    Serial.print("    AngelX: ");Serial.print(analogRead(joyX1));
-    Serial.print("    AngelY: ");Serial.println(analogRead(joyY2));
+    //Serial.print("    AngelX: ");Serial.print(analogRead(joyX1));
+    //Serial.print("    AngelY: ");Serial.println(analogRead(joyY2));
     int data1 = analogRead(joyX1);
     int data2 = analogRead(joyY2);
     radio.write(&data1, sizeof(data1));
     radio.write(&data2, sizeof(data2));
-    Serial.print("Sent data:\t");Serial.print(data1);Serial.print("\t");Serial.print(data2);
+    Serial.print("Sent data:\t");Serial.print(data1);Serial.print("\t");Serial.println(data2);
     time = millis();
   }  
+    if(millis() - interrupt >= 1000) 
+  {
+   data_send.TX_Connect = 5;
+   radio.write(&data_send, sizeof(package_send));
+   interrupt = millis();
+  } 
 }
