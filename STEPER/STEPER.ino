7ef02c47045b81e7ba0ae1895db590f13a4e94ca
KKhang <khangkhuu10727@gmail.com>
2023-08-12 12:37:54 +0700

    some code

--- STEPER/STEPER.ino
+++ STEPER/STEPER.ino
@@ -1,9 +1,9 @@
 #include <Stepper.h>
-const int stepsPerRevolution = 4096;  // hehe
+const int stepsPerRevolution = 4096;  
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 void setup() {
   myStepper.setSpeed(60);
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
--- runWeb/runWeb.ino
+++ runWeb/runWeb.ino
@@ -3,8 +3,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPUpdateServer.h>
-const char* ssid = "MINH KHA";
-const char* password = "0855508877";
+const char* ssid = "Hoang Cuong.";
+const char* password = "99999999";
 
 const char* host = "TEST";
 const char* updatePath = "/update";
@@ -14,75 +14,52 @@ ESP8266HTTPUpdateServer httpUpdater;
 //-----------------------------------------//
 const char MainPage[] PROGMEM = R"=====(
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Trang web giới thiệu mạng xã hội</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-        }
-
-        h1 {
-            text-align: center;
-            margin: 20px 0;
-        }
-
-        .social-icons {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin: 20px;
-        }
-
-        .social-icons a {
-            display: inline-block;
-            margin: 10px;
-            color: #fff;
-            font-size: 24px;
-            width: 40px;
-            height: 40px;
-            text-align: center;
-            line-height: 40px;
-            border-radius: 50%;
-            transition: background-color 0.3s ease;
-        }
-
-        .social-icons a:hover {
-            background-color: #333;
-        }
-
-        /* Hiệu ứng cho biểu tượng mạng xã hội */
-        .social-icons a.github {
-            background-color: #333;
-        }
-
-        .social-icons a.facebook {
-            background-color: #3b5998;
-        }
-
-        .social-icons a.zalo {
-            background-color: #1ebc4e;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>iFrame</title>
+<style>
+    h1{
+        text-align: center;
+    }
+    h3{
+        text-align: right;
+    }
+</style>
 </head>
 <body>
-    <h1>Trang web giới thiệu mạng xã hội</h1>
-
-    <div class="social-icons">
-        <a href="https://github.com/SevKan10" target="_blank" class="github"><i class="fab fa-github"></i></a>
-        <a href="https://www.facebook.com/khang.khangkhuu" target="_blank" class="facebook"><i class="fab fa-facebook-f"></i></a>
-        <a href="https://chat.zalo.me" target="_blank" class="zalo"><i class="fab fa-zalo"></i></a>
-    </div>
-
-    <script src="https://kit.fontawesome.com/f09a.js" crossorigin="anonymous"></script>
+    <h1>
+        <div style="width: 300px; margin-left: auto; margin-right: auto;">
+             Welcome to iFrame
+            <button onclick="window.location.href='button.html'">Click here to continue</button>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+    </h1> 
+    <h3>
+        <a ><b><i>Design by Giai Xuan's student</i></b></a>
+    </h3>
 </body>
 </html>
 
 
+
 )=====";
 
+const char PoPage[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control device</title>
+</head>
+<body>
+    <a>Hello</a>
+</body>
+</html>
+
+)=====";
 int up = 4;
 void setup(void){
   Serial.begin(9600);
