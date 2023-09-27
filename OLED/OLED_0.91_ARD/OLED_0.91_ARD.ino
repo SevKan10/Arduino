c5cb840814225bebfd6d46bc96cf32001c721506
KKhang <khangkhuu10727@gmail.com>
2023-09-27 21:39:01 +0700

    update

--- OLED/OLED_0.91_ARD/OLED_0.91_ARD.ino
+++ OLED/OLED_0.91_ARD/OLED_0.91_ARD.ino
@@ -1,29 +1,30 @@
-  #include <Wire.h>
-  #include <Adafruit_GFX.h>
-  #include <Adafruit_SSD1306.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-  #define SCREEN_WIDTH 128
-  #define SCREEN_HEIGHT 32
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
 
-  Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
   Wire.begin();
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("Không tìm thấy màn hình OLED"));
-    while (true);
+    while (true)
+      ;
   }
   display.clearDisplay();
   display.setTextSize(3);
   display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.println("Hello");
   display.display();
   delay(3000);
 }
 
 void loop() {
- /* display.clearDisplay();
+  /* display.clearDisplay();
   display.setTextSize(3);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0,0);
@@ -38,20 +39,19 @@ void loop() {
   display.display();
   delay(5000);*/
 
-   for (int x = 0; x < 128; x++) {
+  for (int x = 0; x < 128; x++) {
     display.clearDisplay();
-    display.setCursor(x,8);
+    display.setCursor(x, 8);
     display.print("80 pbm");
     display.display();
     delay(10);
   }
-  
-   for (int x = 0; x < 128; x++) {
+
+  for (int x = 0; x < 128; x++) {
     display.clearDisplay();
-    display.setCursor(x,8);
+    display.setCursor(x, 8);
     display.print("90 spo");
     display.display();
     delay(10);
   }
-
 }
