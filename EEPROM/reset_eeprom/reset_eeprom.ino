dd11514efaa2ccd88074dc650b92287192e27792
KKhang <127233909+SevKan10@users.noreply.github.com>
2024-01-10 22:03:47 +0700

    Update reset_eeprom.ino

--- EEPROM/reset_eeprom/reset_eeprom.ino
+++ EEPROM/reset_eeprom/reset_eeprom.ino
@@ -1,8 +1,9 @@
 #include <EEPROM.h>
-
+String PW ="";
 void setup() {
   // Khởi tạo Serial Monitor
   Serial.begin(9600);
+  EEPROM.begin(16); 
   
   // Reset EEPROM
   resetEEPROM();
@@ -14,9 +15,11 @@ void loop() {
 
 void resetEEPROM() {
   // Ghi giá trị 0 vào tất cả các địa chỉ trong EEPROM
-  for (int i = 0; i < EEPROM.length(); i++) {
+  for (int i = 0; i < 4; i++) {
     EEPROM.write(i, 0);
   }
 
   Serial.println("EEPROM đã được reset.");
+  for (int i = 0; i < 4; ++i){PW += char(EEPROM.read(i)); Serial.println(PW.toInt()); }
+
 }
